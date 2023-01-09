fa53bd210fee16ac724ace389d29f01e332b6aed
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-01-09 21:43:55 +0200

    materials

--- LearnOpenGL/assets/shaders/lighting_fs.glsl
+++ LearnOpenGL/assets/shaders/lighting_fs.glsl
@@ -1,9 +1,26 @@
 #version 330 core
 
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 in vec3 FragPos;
 in vec3 Normal;
 
 out vec4 frag_color;
+
+uniform Material material;
+uniform Light light;
 
 uniform vec3 light_pos;
 uniform vec3 view_pos;
@@ -13,23 +30,21 @@ uniform vec3 light_color;
 void main()
 {
     // ambient
-    float ambient_strength = 0.1;
-    vec3 ambient = ambient_strength * light_color;
+    vec3 ambient = light.ambient * material.ambient;
 
     // diffuse
     vec3 norm = normalize(Normal);
     vec3 light_dir = normalize(light_pos - FragPos);
     float diff = max(dot(norm, light_dir), 0.0);
-    vec3 diffuse = diff * light_color;
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     // specular
-    float specular_strength = 0.5;
     vec3 view_dir = normalize(view_pos - FragPos);
     vec3 reflect_dir = reflect(-light_dir, norm);
-    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
-    vec3 specular = specular_strength * spec * light_color;
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);
 
     // result
-    vec3 result = (ambient + diffuse + specular) * object_color;
+    vec3 result = ambient + diffuse + specular;
     frag_color = vec4(result, 1.0);
 }
