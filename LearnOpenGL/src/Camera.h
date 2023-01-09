955c986c436366eeb47fe01bf6e4494a5eff6d0a
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-01-09 21:43:31 +0200

    camera move speed

--- LearnOpenGL/src/Camera.h
+++ LearnOpenGL/src/Camera.h
@@ -20,7 +20,7 @@ enum CameraMovement {
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0;
-const float SPEED = 2.5f;
+const float SPEED = 5.0f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
 
