33cf744c1984b81802890881ea5f14f2380a5c45
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2023-02-12 01:11:21 +0200

    update

--- LearnOpenGL/src/Log.h
+++ LearnOpenGL/src/Log.h
@@ -20,9 +20,9 @@ private:
 #   define LOG_ERROR(...)    ::Log::GetLogger()->error(__VA_ARGS__)
 #   define LOG_FATAL(...)    ::Log::GetLogger()->critical(__VA_ARGS__)
 #else
-#   define LOG_TRACE(...)    ::Log::GetLogger()->trace(__VA_ARGS__)
-#   define LOG_INFO(...)     ::Log::GetLogger()->info(__VA_ARGS__)
-#   define LOG_WARN(...)     ::Log::GetLogger()->warn(__VA_ARGS__)
-#   define LOG_ERROR(...)    ::Log::GetLogger()->error(__VA_ARGS__)
-#   define LOG_FATAL(...)    ::Log::GetLogger()->critical(__VA_ARGS__)
+#   define LOG_TRACE(...)
+#   define LOG_INFO(...)
+#   define LOG_WARN(...)
+#   define LOG_ERROR(...)
+#   define LOG_FATAL(...)
 #endif
